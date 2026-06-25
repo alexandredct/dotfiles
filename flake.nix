4b74b4c2400bf9dc51d8da3ed90aaa38803a42d7
Alexandre Trindade <alexandre.trindade@uerj.br>
2026-06-25 16:05:59 -0300

    feat: configuração inicial do Nix Flakes e Home Manager

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Meus Dotfiles com Nix e Home Manager";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, home-manager, ... }: 
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      homeConfigurations."alexandre" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [ ./home.nix ];
+      };
+    };
+}
